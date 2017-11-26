463de24e7c6985064ef120712fa9d1fe2f879c42
liwangqian <wqgg123@163.com>
2017-11-26 22:09:00 +0800

    chang the test file for moduleAnalyse test.

--- test/test_module.js
+++ test/test_module.js
@@ -2,7 +2,7 @@
 
 const analysis = require('../index');
 
-const filePath = 'F:\\lua\\power_manage\\externel.lua';
+const filePath = './test/texture/test01.lua';
 
 analysis.module(filePath).then(report => {
     console.log(JSON.stringify(report, null, 4));
